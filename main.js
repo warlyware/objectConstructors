15a1699fd70d9fd5e7da60a596edd41a7eb06171
warlyware <danward@gmail.com>
2015-06-26 09:35:31 -0700

    Add full object constructors exercise

--- main.js
+++ main.js
@@ -21,9 +21,36 @@ Bootcamp.prototype.enroll = function(animal) {
   if (this.students.length < this.capacity) {
     this.students.push(animal);
   }
+}
+
+Bootcamp.prototype.findByAge = function(age) {
+  return this.students.filter(function(student) {
+    return student.age < age;
+  });
+}
+
+Bootcamp.prototype.studentGetsJob = function(student) {
+  var students = this.students;
+  students.splice(students.name = ' goodbye ' + this.name + ' I will miss you');
 };
 
-var ryano = new Student('ryano', 'human', 26);
+
+var ryano = new Student('Ryano', 'human', 26);
+var dan = new Student('Dan', 'human', 44);
+var sean = new Student('Sean', 'human', 31);
+var troy = new Student('Troy', 'human', 85);
+var bob = new Student('Bob', 'human', 15);
+var charlie = new Student('Charlie', 'dog', 14);
+
+var studentArr = [ryano, dan, sean, troy, bob, charlie];
+studentArr.forEach(function(student) {
+  codingHouse.enroll(student);
+})
+
+
 codingHouse.enroll(ryano);
 
+codingHouse.studentGetsJob(ryano);
+
 console.log(codingHouse);
+console.log(codingHouse.findByAge(24));
